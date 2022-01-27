8c42fff0b9f73193530b84c1c781b66f3409f309
BogdanDidenko <b.didenko@gmail.com>
2022-01-27 15:39:02 +0200

    undo ds finetune

--- chemberta/finetune/finetune.py
+++ chemberta/finetune/finetune.py
@@ -428,15 +428,14 @@ class FinetuneDatasets:
 class FinetuneDataset(torch.utils.data.Dataset):
     def __init__(self, df, tokenizer, include_labels=True):
 
-        self.encodings = tokenizer(df["smiles"].tolist(), truncation=True, padding=True, return_tensors="pt")
-        self.encodings = self.encodings.to(device)
+        self.encodings = tokenizer(df["smiles"].tolist(), truncation=True, padding=True)
         self.labels = df.iloc[:, 1].values
-        self.include_labels = torch.tensor(include_labels, dtype=torch.float32).to(device)
+        self.include_labels = include_labels
 
     def __getitem__(self, idx):
-        item = {key: val[idx] for key, val in self.encodings.items()}
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
         if self.include_labels and self.labels is not None:
-            item["labels"] = self.labels[idx]
+            item["labels"] = torch.tensor(self.labels[idx], dtype=torch.float32)
         return item
 
     def __len__(self):
